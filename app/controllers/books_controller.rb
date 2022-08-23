f88d12f1a0e24416248724814b9dbb630c219c52
t-fujisaki88 <taku.fujisaki@medpeer.co.jp>
2022-08-23 13:45:09 +0900

    quit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
   # GET /books
   # GET /books.json
   def index
-    @books = Book.page(params[:page]).per(2)
+    @books = Book.order(:name).page(params[:page]).per(2)
   end
 
   # GET /books/1
